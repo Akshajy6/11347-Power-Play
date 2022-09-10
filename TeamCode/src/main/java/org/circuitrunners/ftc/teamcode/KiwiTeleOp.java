7cfd3e4772934be8a695ca4f6d02a306fff69974
Akshajy6 <Akshajy6@gmail.com>
2022-09-10 16:56:39 -0400

    GitHub Desktop fix

---
+++ TeamCode/src/main/java/org/circuitrunners/ftc/teamcode/KiwiTeleOp.java
@@ -0,0 +1,24 @@
+package org.circuitrunners.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class KiwiTeleOp extends LinearOpMode {
+    KiwiDrive drivebase;
+
+    public void runOpMode() throws InterruptedException {
+        DcMotor left = hardwareMap.dcMotor.get("left");
+        DcMotor right = hardwareMap.dcMotor.get("right");
+        DcMotor slide = hardwareMap.dcMotor.get("slide");
+
+        drivebase = new KiwiDrive(left, right, slide);
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            drivebase.drive(gamepad1.left_stick_y, gamepad1.right_stick_y, (gamepad1.left_stick_x + gamepad1.right_stick_x) / 2);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/circuitrunners/ftc/teamcode/ROMecanumTeleOp.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class ROMecanumTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor fl = hardwareMap.dcMotor.get("fl");
+        DcMotor bl = hardwareMap.dcMotor.get("bl");
+        DcMotor fr = hardwareMap.dcMotor.get("fr");
+        DcMotor br = hardwareMap.dcMotor.get("br");
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double flPower = (y + x + rx) / denominator;
+            double blPower = (y - x + rx) / denominator;
+            double frPower = (y - x - rx) / denominator;
+            double brPower = (y + x - rx) / denominator;
+
+            fl.setPower(flPower);
+            bl.setPower(blPower);
+            fr.setPower(frPower);
+            br.setPower(brPower);
+        }
+    }
+}
