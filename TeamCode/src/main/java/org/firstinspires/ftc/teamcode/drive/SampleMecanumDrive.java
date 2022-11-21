3e4ae4971ecfd667a6218af68d762600f78fc7b2
Akshajy6 <Akshajy6@gmail.com>
2022-11-21 14:28:30 -0500

    Tuning RR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -55,7 +55,7 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
 
-    public static double LATERAL_MULTIPLIER = 1;
+    public static double LATERAL_MULTIPLIER = 1.137 * 0.9863;
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
