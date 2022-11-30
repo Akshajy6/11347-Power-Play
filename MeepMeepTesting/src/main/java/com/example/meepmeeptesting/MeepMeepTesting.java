6da0f8eb7e71b295c0671f6306d2070e53778844
ConcaveEgg <justinracel.jr@gmail.com>
2022-11-30 13:44:53 -0500

    Update MeepMeepTesting.java
    Made MeepMeep work i think, testing code

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,21 +16,57 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-36, 64, 180))
-                                .forward(5)
-                                .turn(Math.toRadians(90))
-                                .forward(53)
-                                .strafeLeft(12)
-                                .addDisplacementMarker(55, () -> {
-//                                    l.setPower(0.8);
-//                                    r.setPower(0.8);
-                                })
+                        drive.trajectorySequenceBuilder(new Pose2d(-24, 64, Math.toRadians(180)))
                                 .forward(4)
-                                .addDisplacementMarker(74, () -> {
-//                                    i.setPower(0.8);
-                                })
-                                .back(4)
-                                .build()
+                        .strafeLeft(6)
+                        .turn(Math.toRadians(90))
+                        .forward(38)
+                        .strafeLeft(17)
+                        .addDisplacementMarker(45, () -> {
+//                            l.setPower(0.7);
+//                            r.setPower(0.7);
+                        })
+                        .addDisplacementMarker(64, () -> {
+//                            i.setPower(0.8);
+                        })
+                        .addDisplacementMarker(() -> {
+//                            l.setPower(0);
+//                            r.setPower(0);
+//                            i.setPower(0);
+                        })
+                        .back(2.5)
+                        .turn(Math.toRadians(-90))
+                        .forward(26.45)
+                        .addDisplacementMarker(71, () -> {
+//                            l.setPower(0.6);
+//                            r.setPower(0.6);
+                        })
+                        .addDisplacementMarker(86, ()-> {
+//                            l.setPower(0);
+//                            r.setPower(0);
+                        })
+                        .addDisplacementMarker(91, () -> {
+//                            i.setPower(-0.8);
+                        })
+                        .addDisplacementMarker(96,()->{
+//                            l.setPower(0.7);
+//                            r.setPower(0.7);
+//                            i.setPower(0);
+                        })
+                        .back(29.5)
+                        .turn(Math.toRadians(90))
+                        .forward(3)
+                        .addDisplacementMarker(135,()-> {
+//                            i.setPower(0.8);
+                        })
+                        .addDisplacementMarker(145, () -> {
+//                            i.setPower(0);
+//                            l.setPower(0);
+//                            r.setPower(0);
+                        })
+                        .back(4)
+
+                        .build()
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
