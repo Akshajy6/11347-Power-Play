bfa4911d690e64fdc4636a85d9116e3210432367
ConcaveEgg <justinracel.jr@gmail.com>
2023-02-22 14:17:22 -0500

    Update SyntaxErrorTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumSyntaxError/SyntaxErrorTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumSyntaxError/SyntaxErrorTeleOp.java
@@ -1,16 +1,26 @@
 package org.firstinspires.ftc.teamcode.MecanumSyntaxError;
 
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.DPAD_UP;
+
 import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.PerpetualCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Mecanum20D54D.Mecanum;
 
 @TeleOp
 public class SyntaxErrorTeleOp extends LinearOpMode {
+
+
     //Initializing drivetrain and four bar
     SyntaxErrorMechanisms mechanisms;
     Mecanum drivetrain;
@@ -23,11 +33,12 @@ public class SyntaxErrorTeleOp extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        
+
         //Initializing hardware
         Gamepad p1 = new Gamepad();
-        Gamepad p2 = new Gamepad();
         Gamepad c1 = new Gamepad();
-        Gamepad c2 = new Gamepad();
+        GamepadEx manipulator = new GamepadEx(gamepad2);
 
         DcMotor fl = hardwareMap.dcMotor.get("fl");
         DcMotor fr = hardwareMap.dcMotor.get("fr");
@@ -50,25 +61,24 @@ public class SyntaxErrorTeleOp extends LinearOpMode {
         while (opModeIsActive()) {
             try {
                 p1.copy(c1);
-                p2.copy(c2);
                 c1.copy(gamepad1);
-                c2.copy(gamepad2);
-            } catch (RobotCoreException e) {
+            }
+
+            catch (RobotCoreException e)
+            {
                 e.printStackTrace();
             }
+
             //Mecanum drivetrain code
             if (drivetrain.drive(c1.left_stick_y, -c1.left_stick_x * 1.1, -c1.right_stick_x, p1.right_bumper, c1.right_bumper)) {
                 gamepad1.rumble(250); //Angle recalibrated
             }
-            mechanisms.runManual(-c2.right_stick_y, c2.left_trigger - c2.right_trigger);
-            telemetry.addLine("POS: " + mechanisms.getPosition());
-            telemetry.update();
 
-            if (!p2.dpad_up && c2.dpad_up) {
-                while (mechanisms.getPosition() < HIGH) {
-                    mechanisms.runPID(HIGH);
-                }
-            }
+            mechanisms.runManual(-gamepad2.right_stick_y, gamepad2.left_trigger - gamepad2.right_trigger);
+
+//            intakePosCommand = new IntakePositionCommand(fbpid, manipulator);
+//
+//            fbpid.setDefaultCommand(new PerpetualCommand(intakePosCommand));
         }
     }
 }
